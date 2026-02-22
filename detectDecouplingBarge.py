8fecc1155dc77ba7c057380da56e1e3b273399ed
valeria-vasquez-barros <valeriavasquez1921@gmail.com>
2026-02-21 23:47:13 -0500

    changed avgTemp calculation

--- detectDecouplingBarge.py
+++ detectDecouplingBarge.py
@@ -20,10 +20,9 @@ dataAssist = xr.open_dataset(filepathAssist,decode_times = "true")
 dataLidar = xr.open_dataset(filepathLidar,decode_times="true")
 
 # Grab theta, temp variables from combined assist file
-theta = dataAssist["theta"].sel(time=slice("2024-07-15 00:10:00","2024-07-15 23:50:50"))
-# theta = dataAssist["theta"]
-temp = dataAssist["temperature"].sel(time=slice("2024-07-15 00:10:00","2024-07-15 23:50:50"))
-# temp = dataAssist["temperature"]
+#.sel(time=slice("2024-07-20 00:00:00","2024-07-20 23:50:50"))
+theta = dataAssist["theta"]
+temp = dataAssist["temperature"]
 dTheta = theta.differentiate("height")
 
 # Compare "near-surface" and "hub-height"
@@ -50,18 +49,7 @@ def detect_staticdecoupling(dTheta_surf,dTheta_hub):
         }
 
 events = detect_staticdecoupling(dTheta_surf,dTheta_hub)
-# print(events)
-
-    # found = False
-    
-    # if np.any(logic1):
-    #     print("statically stable near surface and statically unstable near hub at:",times1.values)
-    #     found = True
-    # if np.any(logic2):
-    #     print("statically unstable near surface and statically stable near hub:",times2.values)
-    #     found = True
-    # if not found:
-    #     print("no decoupling detected")
+print(events)
 
 # collect sunrise/sunset info
 location = LocationInfo(latitude=dataAssist.VIP_station_lat, longitude=dataAssist.VIP_station_lon, timezone="UTC")
@@ -70,44 +58,15 @@ s=sun(location.observer, date=date)
 sunrise = s["sunrise"]
 sunset = s["sunset"]
 
-# # plot dTheta along height and time:
-# plt.figure(figsize=(10, 5))
-# dTheta.plot(x="time", y="height", cmap="coolwarm")
-# ax = plt.gca()
-# ax.xaxis.set_major_formatter(mdates.DateFormatter("%H"))  # only show hours
-# ax.axvline(sunrise,color="purple",linestyle="--",linewidth=1.5,label='Sunrise')
-# ax.axvline(sunset,color="black",linestyle="--",linewidth=1.5,label='Sunset')
-# ax.legend(loc="upper right")
-# plt.title("15 July, 2024")
-# plt.xlabel("UTC Time")
-# plt.ylabel("Height (m)")
-# plt.tight_layout()
-# plt.show()
-
 # grab wind speed, wind direction from combined lidar file
-# wind_speed = dataLidar["wind_speed"]
-wind_speed = dataLidar["wind_speed"].sel(time=slice("2024-07-15 00:10:00","2024-07-15 23:50:00"))
-# wind_direction = dataLidar["wind_direction"]
-wind_direction = dataLidar["wind_direction"].sel(time=slice("2024-07-15 00:10:00","2024-07-15 23:50:00"))
+wind_speed = dataLidar["wind_speed"]
+# wind_speed = dataLidar["wind_speed"].sel(time=slice("2024-07-20 00:00:00","2024-07-20 23:50:00"))
+wind_direction = dataLidar["wind_direction"]
+# wind_direction = dataLidar["wind_direction"].sel(time=slice("2024-07-20 00:00:00","2024-07-20 23:50:00"))
 
 # calculate u and v
 uGeo = -wind_speed * np.sin(wind_direction)
 vGeo = -wind_speed * np.cos(wind_direction)
-# sGeo = np.sqrt(uGeo**2+vGeo**2)
-
-# sGeo_surf = sGeo.sel(height=slice(40,60))
-# sGeo_hub = sGeo.sel(height=slice(120,160))
-
-# # plot wind speed along height and time
-# fig,ax = plt.subplots(figsize=(10,5))
-# T,Z = np.meshgrid(uGeo["time"],uGeo["height"],indexing="ij")
-# q = ax.quiver(T[::5],Z[::5],uGeo[::5],vGeo[::5],sGeo[::5])
-# plt.colorbar(q,ax=ax,label="Wind Speed (m/s)")
-# ax.xaxis.set_major_formatter(mdates.DateFormatter("%H"))  # only show hours
-# ax.set_xlabel("UTC Time")
-# ax.set_ylabel("Height (m)")
-# ax.set_title("Wind Vector Field [combined lidar file] on 20 July, 2024")
-# plt.show()
 
 # calculate surface Bulk Richardson number:
 # 1) establish height difference
@@ -122,8 +81,10 @@ deltaTheta_surf = thetasurf_f - thetasurf_i # K
 # 3) change in temperature
 tempsurf_i = temp.sel(height=hsurf_i)
 tempsurf_f = temp.sel(height=hsurf_f)
-dTemp_surf = tempsurf_f - tempsurf_i
-avgTemp_surf = dTemp_surf/dZ_surf + 273.15 # K, apparently this could be a lapse rate?
+# dTemp_surf = tempsurf_f - tempsurf_i
+# avgTemp_surf = dTemp_surf/dZ_surf + 273.15 # K, apparently this could be a lapse rate?
+avgTemp_surf = (tempsurf_i+tempsurf_f)/2 # K
+
 # 4) change in u,v over heights
 usurf_i = uGeo.sel(height=hsurf_i)
 usurf_f = uGeo.sel(height=hsurf_f)
@@ -137,7 +98,6 @@ num2_surf = deltaTheta_surf*dZ_surf
 sGeo_surf = (dU_surf**2+dV_surf**2)
 num3_surf = num2_surf/sGeo_surf
 BulkRi_surf = num1_surf*num3_surf
-# BulkRi_surf = xr.DataArray(bulk_Ri_surf, coords = {"time": dataAssist.time}, dims = ("time"))
 
 # calculate hub Bulk Richardson number:
 # 1) establish height difference
@@ -154,7 +114,8 @@ deltaTheta_hub = thetahub_f - thetahub_i # K
 temphub_i = temp.sel(height=hhub_i)
 temphub_f = temp.sel(height=hhub_f)
 dTemp_hub = temphub_f - temphub_i
-avgTemp_hub = dTemp_hub/dZ_hub + 273.15 # K, apparently this could be a lapse rate?
+# avgTemp_hub = dTemp_hub/dZ_hub + 273.15 # K, apparently this could be a lapse rate?
+avgTemp_hub = (tempsurf_i+tempsurf_f)/2 # K
 
 # 4) change in u,v over heights
 uhub_i = uGeo.sel(height=hhub_i)
@@ -170,7 +131,6 @@ num2_hub = deltaTheta_hub*dZ_hub
 sGeo_hub = (dU_hub**2+dV_hub**2)
 num3_hub = num2_hub/sGeo_hub
 BulkRi_hub = num1_hub*num3_hub
-# BulkRi_hub = xr.DataArray(bulk_Ri_hub, coords = {"time": dataAssist.time}, dims = ("time"))
 
 def detect_dynamicdecoupling(BulkRi_surf,BulkRi_hub):
     
@@ -193,58 +153,30 @@ def detect_dynamicdecoupling(BulkRi_surf,BulkRi_hub):
 devents = detect_dynamicdecoupling(BulkRi_surf,BulkRi_hub)
 print(devents)
 
-
-
-
-
-
-# plot Bulk Richardson number over time
-# BulkRi.plot()
+# # plot dTheta along height and time:
+# plt.figure(figsize=(10, 5))
+# dTheta.plot(x="time", y="height", cmap="coolwarm")
 # ax = plt.gca()
 # ax.xaxis.set_major_formatter(mdates.DateFormatter("%H"))  # only show hours
-# ax.axhline(0.25, color="orange", linestyle="--", label='Critical Ri')
 # ax.axvline(sunrise,color="purple",linestyle="--",linewidth=1.5,label='Sunrise')
 # ax.axvline(sunset,color="black",linestyle="--",linewidth=1.5,label='Sunset')
-# ax.legend(loc="lower right")
-# plt.title("20 July, 2024")
+# ax.legend(loc="upper right")
+# plt.title("15 July, 2024")
 # plt.xlabel("UTC Time")
-# plt.ylabel("Bulk Richardson number between 60-200 m")
+# plt.ylabel("Height (m)")
 # plt.tight_layout()
 # plt.show()
 
-# # calculate Bulk Richardson number:
-# # 1) establish height difference
-# g = 9.81
-# h_i = 60
-# h_f = 200
-# deltaZ = h_f-h_i
-
-# # 2) change in potential temperature
-# theta_i = theta.sel(height=h_i)
-# theta_f = theta.sel(height=h_f)
-# deltaTheta = theta_f.values - theta_i.values # K
-
-# # 3) change in temperature
-# temp_i = temp.sel(height=h_i)
-# temp_f = temp.sel(height=h_f)
-# deltaTemp = temp_f.values - temp_i.values
-# avgTemp = deltaTemp/deltaZ + 273.15 # K
-
-# # 4) change in u,v over heights
-# u_i = uGeo.sel(height=h_i)
-# u_f = uGeo.sel(height=h_f)
-# deltaU = u_f.values - u_i.values
-# v_i = vGeo.sel(height=h_i)
-# v_f = vGeo.sel(height=h_f)
-# deltaV = v_f.values - v_i.values
-
-# # 5) final calculation
-# numerator1 = g/avgTemp
-# numerator2 = deltaTheta*deltaZ
-# sGeo = (deltaU**2+deltaV**2)
-# numerator3 = numerator2/sGeo
-# bulk_Ri = numerator1*numerator3
-# BulkRi = xr.DataArray(bulk_Ri, coords = {"time": dataAssist.time}, dims = ("time"))
+# # plot wind speed along height and time
+# fig,ax = plt.subplots(figsize=(10,5))
+# T,Z = np.meshgrid(uGeo["time"],uGeo["height"],indexing="ij")
+# q = ax.quiver(T[::5],Z[::5],uGeo[::5],vGeo[::5],sGeo[::5])
+# plt.colorbar(q,ax=ax,label="Wind Speed (m/s)")
+# ax.xaxis.set_major_formatter(mdates.DateFormatter("%H"))  # only show hours
+# ax.set_xlabel("UTC Time")
+# ax.set_ylabel("Height (m)")
+# ax.set_title("Wind Vector Field [combined lidar file] on 20 July, 2024")
+# plt.show()
 
 # # plot Bulk Richardson number over time
 # BulkRi.plot()
